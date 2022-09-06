c41814a3381691e42279533185c055f15941b776
say <say@DESKTOP-N4LAFSI>
2022-09-06 21:48:34 +0900

    [Add]practice

---
+++ java-practice/src/Hairetu2.java
@@ -0,0 +1,16 @@
+
+public class Hairetu2 {
+
+	public static void main(String[] args) {
+		int[] date = new int[4];
+
+		date[0] = 90;
+		date[1] = 80;
+		date[2] = 100;
+		date[3] = 90;
+
+		System.out.println("1人目は" + date[0] + "点");
+
+	}
+
+}
